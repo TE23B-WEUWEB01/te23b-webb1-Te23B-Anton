08d6c3d93331044e99b6236c45a4277b6d450ddf
Anton Faren <Anton.faren@elev.ga.ntig.se>
2025-03-13 11:07:34 +0100

    tränat på js

---
+++ kapitel-8/intro/script.js
@@ -0,0 +1,15 @@
+// var ålder = 99;
+// var tal1 = 12;
+// var tal2 = 24;
+// var summa = tal1 + tal2;
+// alert(tal1 + " + " + tal2 + " = " + summa);
+
+var förnamn = "Anton";
+var efternamn = "Faren";
+var helaNamnet = förnamn + " " + efternamn;
+var hobby = "spela";
+kostnad = 1200;
+
+var mening = helaNamnet + " s hobby är att " + hobby + ".\nMin hobby kostar " +
+kostnad + " kr/år."
+alert(mening);
