3c7a138bbb16fde60a49b6ce574f6f0326c924aa
Anton Faren <Anton.faren@elev.ga.ntig.se>
2025-03-14 14:03:48 +0100

    Tränat på promt och att sktiva i webbfönstret

---
+++ kapitel-8/grunder/script.js
@@ -0,0 +1,42 @@
+// var dag = "fredag";
+// alert(dag);
+// console.log("idag är det " + dag);
+
+// var är du född?
+// var stad = prompt("vad är du född?");
+// console.log("Du är född i " + stad);
+
+// var gissning = prompt ("Är det okej att fuska på prov?");
+// if (gissning == "nej") {
+//  alert("du är människa");   
+// } else {
+//     alert("Du är en robot");
+// }
+
+// var slumptal1 =Math.round(Math.random() * 100);
+// var slumptal2 =Math.round(Math.random() * 100);
+// var facit = slumptal1 + slumptal2;
+
+// var gissning = prompt("vad är " + slumptal1 + " + " + slumptal2 + "?")
+// if (gissning == facit) {
+
+// document.writeln("<h1>Du är en människa! <h1>");
+
+// } else {
+//     document.writeln("<h1>Du är en människa! <h1>");
+// }
+
+
+// var tal1 = prompt ("Ange ett tal1");
+// var tal2 = prompt ("Ange ett tal2");
+// var summa = Number(tal1) + Number(tal2);
+
+// document.writeln("Summan är " + summa);
+
+var brutto = prompt("vad är din lön före skatt?");
+var skatt = prompt("vad är din skatt?");
+
+var summa = Nummer(brutto) * (100 - Nummer(skatt)) / 100;
+
+document.writeln("Din inkomst efter skatt är " + inkomst + "med " + skatt + "% i skatt.");
+
