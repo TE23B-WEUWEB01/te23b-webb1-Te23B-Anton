526cfbee58c0b3fcadfc83f158df67c7735f4eb6
Anton Faren <Anton.faren@elev.ga.ntig.se>
2025-03-20 11:11:49 +0100

    mera java scrift uppgifter

---
+++ kapitel-8/meragrunder/script.js
@@ -0,0 +1,51 @@
+
+// uppgift 1
+// var age = prompt("Hur gammal är du?");
+// age = parseInt(age);
+// if (age >= 15) {
+//     document.write("Du får se filmen!");
+//     console.log("Du får se filmen!");
+// } else {
+//     document.write("Du är för ung!");
+//     console.log("Du är för ung!");
+// }
+
+// uppgift 2
+// var temperatur = prompt("Hur många grader är det ute?");
+
+// temperatur = parseFloat(temperatur);
+
+// if (temperatur > 20) {
+//     document.write("Det är varmt ute!");
+//     console.log("Det är varmt ute!");
+// } else {
+//     document.write("Det är kallt ute!");
+//     console.log("Det är kallt ute!");
+// }
+
+
+
+// uppgift 3
+var Favo = prompt ("vad är ditt favorit uppgift?");
+
+if (favo.toLowerCase() == "hund") {
+    document.write("Jag gillar också Hundar!");
+    console.log("Jag gillar också hundar!");
+    
+}
+else {
+    document.write("Jag gillar inte djuret!");
+    console.log("Jag gillar inte djuret!");
+}
+
+//uppgift 4
+var pris = prompt("Hur mycket handlar du för?");
+
+if (pris > 300) {
+    document.write("du får rabatt!");
+    console.log("du får rabatt!");
+}
+else {
+    document.write("ingen rabbat idag");
+    console.log("ingen rabbat idag");
+}
