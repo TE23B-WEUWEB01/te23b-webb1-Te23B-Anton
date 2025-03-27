0c4ca97b73bc145708952020c479cc27cc2e0b3e
Anton Faren <Anton.faren@elev.ga.ntig.se>
2025-03-27 11:08:30 +0100

    tränat på queryselector

---
+++ kapitel-8/interaktiv/script.js
@@ -0,0 +1,21 @@
+
+function darkpage() {
+
+    document.body.style = "background: #000; color: #FFF;";
+    document.querySelector("h1").style = "Font-size: 100px; color: red;";
+}
+
+function whitepage() {
+
+    document.body.style = "background: #FFF: color: #000:";
+    document.querySelector("h1").style = "Font-size: 50px; color: blue;";
+}
+
+function konstigRubrik() {
+
+    document.querySelector("h1").style = "text-decoration: dotted; text-shadow: 5px 5px 5px green; Rotate: 180deg; text-decoration: underline black;";
+}
+function Visatext() {
+    document.querySelector("p").style = "visibility: visible;";
+    
+}
